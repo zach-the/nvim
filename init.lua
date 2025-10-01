19a451dc811c96195a2b52aa4d81ed251f180398
zach-the <zachzbennett@gmail.com>
2025-10-01 15:07:31 -0700

    small change

--- init.lua
+++ init.lua
@@ -157,6 +157,7 @@ require("lazy").setup({
     {
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
+        render_modes = { 'n', 'c', 't', 'i' },
         code = {
           sign = false,
           width = "block",
@@ -173,20 +174,6 @@ require("lazy").setup({
       ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
       config = function(_, opts)
         require("render-markdown").setup(opts)
-        Snacks.toggle({
-          name = "Render Markdown",
-          get = function()
-            return require("render-markdown.state").enabled
-          end,
-          set = function(enabled)
-            local m = require("render-markdown")
-            if enabled then
-              m.enable()
-            else
-              m.disable()
-            end
-          end,
-        }):map("<leader>um")
       end,
     },
 
